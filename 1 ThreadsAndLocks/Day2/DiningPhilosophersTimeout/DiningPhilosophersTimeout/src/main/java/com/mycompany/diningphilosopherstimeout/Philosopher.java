f76184990a96eefc9dd335de6f2c16459525292b
Lukas Woodtli <woodtli.lukas@gmail.com>
2014-12-25 22:16:04 +0100

    Improve 'Dining Philosophers Problem' with timeout
    The actions of the philosophers is printed to the console.

--- 1 ThreadsAndLocks/Day2/DiningPhilosophersTimeout/DiningPhilosophersTimeout/src/main/java/com/mycompany/diningphilosopherstimeout/Philosopher.java
+++ 1 ThreadsAndLocks/Day2/DiningPhilosophersTimeout/DiningPhilosophersTimeout/src/main/java/com/mycompany/diningphilosopherstimeout/Philosopher.java
@@ -16,12 +16,16 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Philosopher extends Thread {
     private ReentrantLock leftChopstick, rightChopstick;
     private Random random;
+    private static int philosophersTotalNo = 0;
+    private int thisPhilosopherNo;
     
     public Philosopher(ReentrantLock leftChopstick,
             ReentrantLock rightChopstick) {
         this.leftChopstick = leftChopstick;
         this.rightChopstick = rightChopstick;
         random = new Random();
+        thisPhilosopherNo = philosophersTotalNo;
+        ++philosophersTotalNo;
     }
     
     public void run() {
@@ -29,17 +33,22 @@ public class Philosopher extends Thread {
             while (true) {
                 Thread.sleep(random.nextInt(1000)); // think for a while
                 leftChopstick.lock();
+                System.out.println("Philosopher " + thisPhilosopherNo + " (" +
+                        this + ") " + "picked up left chopstick");
                 try {
                     if (rightChopstick.tryLock(1000, TimeUnit.MICROSECONDS)) {
-                        // got right chopstick
+                        System.out.println("Philosopher " + thisPhilosopherNo + " (" +
+                            this + ") " + "picked up right chopstick");
                         try {
                             Thread.sleep(random.nextInt(1000));
-                            // Eat for a while
+                            System.out.println("Philosopher " + thisPhilosopherNo +
+                                    " (" + this + ") " + "eats for a while");
                         } finally {
                             rightChopstick.unlock();
                         }
                     } else {
-                        // Didn't get the right chopstick - give up and go back to thinking
+                        System.out.println("Philosopher " + thisPhilosopherNo +
+                                    " (" + this + ") " + "didn't get chopsticks");
                     }
                         
                 } finally {
