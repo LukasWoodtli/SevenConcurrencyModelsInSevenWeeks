d553fa9d171df5340b938f9bac530ef1fceaeb32
Lukas Woodtli <woodtli.lukas@gmail.com>
2014-12-30 21:33:24 +0100

    Better HTTP Downloader example with CopyOnWriteArrayList
    This example uses CopyOnWriteArrayList instead of only a simple ArraList.
    
    Threads and Locks: Day 3

---
+++ 1 ThreadsAndLocks/Day3/HttpDownloadBetter/src/main/java/com/mycompany/httpdownload/Downloader.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.httpdownload;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+
+/**
+ *
+ * @author Boot
+ */
+public class Downloader extends Thread {
+    private InputStream in;
+    private OutputStream out;
+    private CopyOnWriteArrayList<ProgressListener> listeners;
+    
+    public Downloader(URL url, String outputFilename) throws IOException {
+        in  = url.openConnection().getInputStream();
+        out = new FileOutputStream(outputFilename);
+        listeners = new CopyOnWriteArrayList<ProgressListener>();
+    }
+    
+    public synchronized void addListener(ProgressListener listener) {
+        listeners.add(listener);
+    }
+    
+    public synchronized void removeListener(ProgressListener listener) {
+        listeners.remove(listener);
+    }
+    
+    public synchronized void updateProgress(int n) {
+        for (ProgressListener listener: listeners) {
+            listener.onProgress(n);
+        }
+    }
+    
+    public void run() {
+        int n = 0;
+        int total = 0;
+        byte[] buffer = new byte[1024];
+        
+        try {
+            while ((n = in.read(buffer)) != -1) {
+                out.write(buffer, 0, n);
+                total += n;
+                updateProgress(total);
+            }
+            out.flush();
+        } catch (IOException e) {}
+    }
+}
---
+++ 1 ThreadsAndLocks/Day3/HttpDownloadBetter/src/main/java/com/mycompany/httpdownload/ProgressListener.java
@@ -0,0 +1,15 @@
+package com.mycompany.httpdownload;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Boot
+ */
+interface ProgressListener {
+    public void onProgress(int current);
+}
